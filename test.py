bd1944bc2520038533335cdb211780b2e529383b
Yuntao  Ma <ma.yunt@northeastern.edu>
2021-03-28 21:38:52 -0400

    remove testing

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ import bos_crime_vis_tool as vis_tool
 
 df = vis_tool.utils.import_data()
 
-vis_tool.utils.test(df)
+print(vis_tool.utils.get_range(df, 'Lat'))
+yearOptions = list(df['YEAR'].unique().astype('str'))
+yearOptions.append('all')
+print(yearOptions)
+print(type(yearOptions))
